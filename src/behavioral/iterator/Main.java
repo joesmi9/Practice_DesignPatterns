647aeca6ba1ef52065b0f413cf86dedcd611bea2
Joe Smith <joesmith@Joes-MBP.home>
2022-04-19 18:02:11 -0400

    feat(iterator): implementation

---
+++ src/behavioral/iterator/Main.java
@@ -0,0 +1,18 @@
+package src.behavioral.iterator;
+
+public class Main {
+
+    public static void main(String[] args) {
+        var history = new BrowserHistory();
+        history.push("a");
+        history.push("b");
+        history.push("c");
+
+        Iterator iterator = history.createIterator();
+        while(iterator.hasNext()){
+            var url = iterator.current();
+            System.out.println(url);
+            iterator.next();
+        }
+    }
+}
