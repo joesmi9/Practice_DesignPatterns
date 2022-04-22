795ad4c81d978af76f4c05e0dc7e1b41498619eb
Joe Smith <joesmith@Joes-MBP.home>
2022-04-21 21:13:41 -0400

    feat/mediator

---
+++ src/behavioral/mediator/Main.java
@@ -0,0 +1,9 @@
+package src.behavioral.mediator;
+
+public class Main {
+    public static void main(String[] args) {
+        var dialog = new ArticlesDialogBox();
+
+        dialog.simulateUserInteraction();
+    }
+}
